6d79ccd409e3befb9168f4afa6066b89ffb6dfba
VieziDev <matheusviezi@gmail.com>
2022-08-24 09:16:39 -0300

    First Commit

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index() {
+    // Listar todos os registros
+  }
+
+  show() {
+    // Obter UM registro
+  }
+
+  store() {
+    // Criar novo registro
+  }
+
+  update() {
+    // Deletar um registro
+  }
+
+  delete() {
+    // Deletar um registro
+  }
+}
+
+module.exports = new ContactController();
